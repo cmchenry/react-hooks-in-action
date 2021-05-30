a680eb98f20f3e3ff4d57514391212c4e3ff8f85
Chris McHenry <cmchenry@orientedsoftware.com>
2021-05-30 16:07:12 -0600

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,52 @@
+import {
+  BrowerRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+import '../App.css';
+
+import { FaCalendarAlt, FaDoorOpen, FaUsers } from 'react-icons/fa';
+
+import BookablesPage from './Bookables/BookablesPage';
+import BookingsPage from '../Bookings/BookingsPage';
+import UsersPage from './Users/UsersPage';
+import UserPicker from './Users/UserPicker';
+
+export default function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/bookings' className='btn btn-header'>
+                  <FaCalendarAlt />
+                  <span>Bookings</span>
+                </Link>
+                <Link to='/bookables/' className='btn btn-header'>
+                  <FaDoorOpen />
+                  <span>Bookables</span>
+                </Link>
+                <Link to='/users' className='btn btn-header'>
+                  <FaUsers />
+                  <span>Users</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <UserPicker />
+        </header>
+
+        <Routes>
+          <Route path='/bookings' element={<BookingsPage />} />
+          <Route path='/bookables' element={<BookablesPage />} />
+          <Route path='/users' element={<UsersPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
